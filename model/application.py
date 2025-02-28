8e926c3ed40b30cb87562d31f47ceb986b3ce865
Ilia Shebanov <ilya.shebanoff@gmail.com>
2025-02-28 18:27:47 +0200

    add autoformat

--- model/application.py
+++ model/application.py
@@ -2,6 +2,7 @@ from model.pages.registration_page import RegistrationPage
 from model.components.left_panel import LeftPanel
 from model.pages.simple_registration_page import SimpleUserRegistrationPage
 
+
 class Application:
     def __init__(self):
         self.registration = RegistrationPage()
